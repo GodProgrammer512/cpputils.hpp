7ca307fb61987af89ae65cd590a2bd28e299bb52
A Random Guy In The World... <patezinhoverde@gmail.com>
2025-04-12 15:13:28 -0300

    Update cpputils.hpp

--- src/cpputils.hpp
+++ src/cpputils.hpp
@@ -3,28 +3,30 @@
 
 
 // Variables:
-const unsigned char base_terminal[6]{"\033[m"}, bold[7]{"\033[1m"}, base_terminal_bold[13]{"\033[m\033[1m"}, red_color[8]{"\033[31m"}, green_color[8]{"\033[32m"}, yellow_color[8]{"\033[33m"}, blue_color[8]{"\033[34m"}, cyan_color[8]{"\033[36m"};
+const unsigned char base_terminal[]{"\033[m"}, bold[]{"\033[1m"}, base_terminal_bold[]{"\033[m\033[1m"}, red_color[]{"\033[31m"}, green_color[]{"\033[32m"}, yellow_color[]{"\033[33m"}, blue_color[]{"\033[34m"}, cyan_color[]{"\033[36m"};
 
 // Classes and functions:
-class CppUtils{
-
+class CppUtils
+{
   public:
-  class Terminal{
-    public:
-    static void clear_terminal(){
-      printf("\033[2J\033[H");
-      fflush(stdout);
-    }
-  };
+    class Terminal
+    {
+      public:
+        static void clear_terminal()
+        {
+          printf("\033[2J\033[H");
+          fflush(stdout);
+        }
+    };
 
-  // Functions:
-  static unsigned char verify_os(){
-    #ifdef _WIN32 // For Windows.
-      return 1u;
-    #elif defined(__linux__) // For Linux.
-      return 2u;
-    #else // For another OS.
-      return 0u;
-    #endif
-  }
+    // Functions:
+    static unsigned char verify_os(){
+      #ifdef _WIN32 // For Windows.
+        return 1u;
+      #elif defined(__linux__) // For Linux.
+        return 2u;
+      #else // For another OS.
+        return 0u;
+      #endif
+    }
 };
